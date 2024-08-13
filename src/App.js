35e3f88d0e735fd89f2363bba7190f086826bacb
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-13 15:18:58 +0700

    feat: Implement Fetch API calls (GET, PUT, POST) in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import FootballHighlights from '~/components/FootballHighlight';
+import { useState, useEffect } from 'react';
 import './App.css';
-import { Typography } from '@mui/material';
+import { Box, Button, Container, List, ListItem, Typography } from '@mui/material';
 
 const App = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // get api Implement
+        const url = 'https://jsonplaceholder.typicode.com/comments';
+        fetch(url, {
+            headers: {
+                companyId: '47856784365874387',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log('jsonnnn', json);
+                setData(json);
+            })
+            .catch((e) => {
+                console.log('e', e);
+            });
+    }, []);
+    // post and put api implement
+    const postPutEvent = () => {
+        const data = {
+            id: '501',
+            name: 'Seema',
+            mobile: '767676786',
+            designation: 'developer',
+            pin: '45678',
+        };
+        const url = data.id
+            ? `https://jsonplaceholder.typicode.com/comments/${data.id}`
+            : 'https://jsonplaceholder.typicode.com/comments';
+        fetch(url, {
+            method: data.id ? 'PUT' : 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => {
+                console.log('response', response);
+                if (response.status === 200) {
+                    alert('success');
+                }
+            })
+            .catch((e) => {
+                console.log('e', e);
+            });
+    };
     return (
-        <div className="App">
-            <Typography
-                variant="h3"
-                component="h3"
-                sx={{
-                    color: '#1976d2',
-                    fontWeight: 'bold',
-                    textAlign: 'center',
-                }}
-            >
-                Football Highlights
-            </Typography>
-            <FootballHighlights />
-        </div>
+        <Container maxWidth="sm">
+            <Box my={4}>
+                <Typography variant="h2" component="h2" gutterBottom>
+                    Welcome
+                </Typography>
+                <List>
+                    {data.map((item) => (
+                        <ListItem key={item.id}>
+                            <Typography variant="body1">{item.email}</Typography>
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
+            <Button outline onClick={postPutEvent}>
+                Submit
+            </Button>
+        </Container>
     );
 };
 
